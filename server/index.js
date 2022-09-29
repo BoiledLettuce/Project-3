f2b8472d58a828eee3fd8a710bccb5d12cd132c4
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-09-28 20:57:55 -0700

    crazy coding stuff

--- server/index.js
+++ server/index.js
@@ -7,13 +7,15 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://LazySpinach:LazySpinach123@cluster0.xukxsh7.mongodb.net/?retryWrites=true&w=majority';
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://BoiledLettuce:123@cluster0.xukxsh7.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
