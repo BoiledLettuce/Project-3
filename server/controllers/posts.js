cbb3b3bd8273dd67e0cb4169e1dd9253fa363c1c
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-09-29 18:39:26 -0700

    code things

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -58,10 +58,23 @@ export const deletePost = async (req, res) => {
 
 
 
-    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that dern sure id');
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that dern sure id');
 
 
     await PostMessage.findByIdAndRemove(id);
 
+    console.log('DELETE DERN SURE');
+
     res.json( { message: 'Post DERN SURE deleted'});
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that DERN SURE id');
+
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1.1 }, { new: true })
+
+    res.json(updatedPost);
 }
