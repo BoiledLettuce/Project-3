184bed71423c5792dc195fc2678d471a26a11406
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-10-03 13:41:30 -0700

    hmm

---
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PostDetails = () => {
+    console.log('POST FUCK YOU');
+    
+  return (
+    <div>
+        PostDetails
+        </div>
+  )
+}
+
+export default PostDetails
