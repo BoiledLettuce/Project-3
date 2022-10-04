63390b01a31ecc599259b574710cb83af55909b0
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-10-04 01:47:13 -0700

    Updates

---
+++ precache-manifest.b7569820d8aef3aa2811bcf6aa2bacd6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af41dec4ac39bc37a62f3dcd05f47751",
+    "url": "/Project-3/index.html"
+  },
+  {
+    "revision": "37794a263dd71202285a",
+    "url": "/Project-3/static/css/main.8f0b4bbc.chunk.css"
+  },
+  {
+    "revision": "17b5468e9d258bd78a97",
+    "url": "/Project-3/static/js/2.f05e9bd7.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/Project-3/static/js/2.f05e9bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37794a263dd71202285a",
+    "url": "/Project-3/static/js/main.70586580.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Project-3/static/js/main.70586580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93b1970d5c4b4b81ddbe",
+    "url": "/Project-3/static/js/runtime-main.d2ae4121.js"
+  },
+  {
+    "revision": "889bc455a3631743d8d63f69d13533c4",
+    "url": "/Project-3/static/media/Creatures_logo.889bc455.png"
+  }
+]);
